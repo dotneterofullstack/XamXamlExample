770b0ba039696f2bdbebd01868a69d4d63c8497a
Omar Garcia Espiritu <omar.garcia.developer@hotmail.com>
2017-09-14 12:33:24 -0500

    Agregar archivos de proyecto.

---
+++ XamlExample/XamlExample/Views/XamlPlusCodePage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamlExample.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class XamlPlusCodePage : ContentPage
+    {
+        public XamlPlusCodePage()
+        {
+            InitializeComponent();
+        }
+
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            valueLabel.Text = args.NewValue.ToString("F3");
+        }
+
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            Button button = (Button)sender;
+            await DisplayAlert("Clicked", $"The button '{button.Text}' has been clicked", "Ok");
+        }
+    }
+}
